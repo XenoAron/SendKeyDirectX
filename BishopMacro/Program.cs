3e3139a1aa7a151121f40fe7ed27d84811f7c77f
Ashton Yoon <y109384@naver.com>
2018-10-11 23:11:20 +0900

    remove git folder from interceptor

---
+++ BishopMacro/Program.cs
@@ -0,0 +1,45 @@
+﻿using Interceptor;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BishopMacro
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        static void Main(string[] args)
+        {
+            // 포커스 잡기
+            IntPtr hWnd = FindWindow(null, "MapleStory");
+
+            if (!hWnd.Equals(IntPtr.Zero))
+                SetForegroundWindow(hWnd);
+            
+            Input input = new Input
+            {
+                KeyboardFilterMode = KeyboardFilterMode.All
+            };
+
+            input.Load();
+
+            Thread.Sleep(3000);
+
+            input.SendKey(Keys.D);
+            input.Unload();
+
+            Thread.Sleep(1000);
+            Console.WriteLine("Ended");
+        }
+    }
+}
